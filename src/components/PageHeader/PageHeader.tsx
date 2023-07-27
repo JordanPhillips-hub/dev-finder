601ef6c092d27cff15e091c1c9a6572552e660b5
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-26 22:51:07 -0600

    Create jsx for PageHeader component

---
+++ src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PageHeader: React.FC = () => {
+  return (
+    <div>
+      <h1>devfinder</h1>
+      <div>
+        <span>Dark</span>
+        {/* Moon Icon */}
+      </div>
+    </div>
+  );
+};
+
+export default PageHeader;

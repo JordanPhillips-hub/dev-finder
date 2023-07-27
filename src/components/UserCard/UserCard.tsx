377061f72d50901699a0f16d16d83878a4181b46
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-27 17:32:50 -0600

    Create rest of UserCard props and jsx

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -6,6 +6,13 @@ export interface UserCardProps {
   created_at: string;
   login: string;
   bio: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  location: string;
+  twitter_username: string;
+  blog: string;
+  company: string;
 }
 
 const UserCard: React.FC<UserCardProps> = ({
@@ -14,6 +21,13 @@ const UserCard: React.FC<UserCardProps> = ({
   created_at,
   login,
   bio,
+  public_repos,
+  followers,
+  following,
+  location,
+  twitter_username,
+  blog,
+  company,
 }) => {
   return (
     <main>
@@ -26,10 +40,47 @@ const UserCard: React.FC<UserCardProps> = ({
           <small>{created_at}</small>
         </div>
         <small>{login}</small>
-        <p>{bio}</p>
+        <p>{bio ?? "No User Bio"}</p>
       </section>
 
-      <section></section>
+      <section>
+        <div>
+          <small>Repos</small>
+          <p>{public_repos}</p>
+        </div>
+        <div>
+          <small>Followers</small>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <small>Following</small>
+          <p>{following}</p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div>
+            {/* icon */}
+            <small>{location ?? "Location Unavailable"}</small>
+          </div>
+          <div>
+            {/* icon */}
+            <small>{twitter_username ?? "Twitter Unavailable"}</small>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            {/* icon */}
+            <small>{blog ?? "Blog Unavailable"}</small>
+          </div>
+          <div>
+            {/* icon */}
+            <small>{company ?? "Company Unavailable"}</small>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };

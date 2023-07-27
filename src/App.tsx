905c56da7d9eade985b86299c46e8f3cccd97488
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-27 12:16:24 -0600

    Refactor App
    Create state for user, isLoading, errMsg
    
    Update getUser function to update state accordingly
    
    Create is loading div rendered
    if isLoading is true
    
    Update the UserCard to display api data if user is not null

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { ChangeEvent, useState } from "react";
 import axios from "axios";
 import PageHeader from "./components/PageHeader/PageHeader";
 import SearchBar from "./components/SearchBar/SearchBar";
+import UserCard, { UserCardProps } from "./components/UserCard/UserCard";
 
 function App() {
-  const [searchInput, setSearchInput] = useState<string | null>("");
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [user, setUser] = useState<null | UserCardProps>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errMsg, setErrMsg] = useState<string>("");
 
   const handleSearch = ({
     target: { value },
@@ -14,19 +18,37 @@ function App() {
 
   async function getUser() {
     try {
+      setIsLoading(true);
       const { data } = await axios.get(
         `https://api.github.com/users/${searchInput}`
       );
-      console.log(data);
-    } catch (error) {
-      console.log(error);
+      setUser(data as UserCardProps);
+      setErrMsg("");
+      setIsLoading(false);
+      console.log(user);
+    } catch (err) {
+      setErrMsg("User Not Found");
+      setIsLoading(false);
+      setUser(null);
     }
   }
 
   return (
     <>
+      {isLoading && <div>Loading...</div>}
       <PageHeader />
-      <SearchBar onChange={handleSearch} onClick={getUser} />
+      <SearchBar onChange={handleSearch} onClick={getUser} errMsg={errMsg} />
+      {user !== null ? (
+        <UserCard
+          avatar_url={user.avatar_url}
+          name={user.name}
+          created_at={user.created_at}
+          login={user.login}
+          bio={user.bio ? user.bio : "No User Bio"}
+        />
+      ) : (
+        <div>Cannot find user</div>
+      )}
     </>
   );
 }

6302f9f2a2adc0e92e44e41ea48239f59f4cf244
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-28 10:20:31 -0600

    Apply PageHeader styles

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -3,11 +3,11 @@ import { BsMoonFill } from "react-icons/bs";
 
 const PageHeader: React.FC = () => {
   return (
-    <header>
-      <h1>devfinder</h1>
-      <div>
-        <span>Dark</span>
-        <BsMoonFill />
+    <header className="flex justify-between text-slate-900 font-bold">
+      <h1 className="text-2.2xl">devfinder</h1>
+      <div className="flex items-center gap-4">
+        <span className="text-sm tracking-wider">Dark</span>
+        <BsMoonFill className="w-5 h-5" />
       </div>
     </header>
   );

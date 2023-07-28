72ccfcf83e8812697779ac22bde6debbe9b89349
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-28 11:33:49 -0600

    Refactor PageHeader styles

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -3,10 +3,10 @@ import { BsMoonFill } from "react-icons/bs";
 
 const PageHeader: React.FC = () => {
   return (
-    <header className="flex justify-between text-slate-900 font-bold">
-      <h1 className="text-2.2xl">devfinder</h1>
-      <div className="flex items-center gap-4">
-        <span className="text-sm tracking-wider">Dark</span>
+    <header className="flex justify-between font-bold mb-8">
+      <h1 className="text-slate-800 text-2.2xl">devfinder</h1>
+      <div className="text-slate-500 flex items-center gap-4">
+        <span className=" text-sm tracking-wider">Dark</span>
         <BsMoonFill className="w-5 h-5" />
       </div>
     </header>

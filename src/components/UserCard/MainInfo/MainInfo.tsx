bba8e5e0e8273577c4a5d80844155052abcd18fe
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-27 18:41:03 -0600

    Create MainInfo component

---
+++ src/components/UserCard/MainInfo/MainInfo.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface MainInfoProps {
+  avatar_url: string;
+  name: string;
+  created_at: string;
+  login: string;
+  bio: string;
+}
+
+const MainInfo: React.FC<MainInfoProps> = ({
+  avatar_url,
+  name,
+  created_at,
+  login,
+  bio,
+}) => {
+  return (
+    <section>
+      <img src={avatar_url} alt="user profile" />
+
+      <div>
+        <header>
+          <h2>{name}</h2>
+        </header>
+        <small>{created_at}</small>
+      </div>
+      <small>{login}</small>
+      <p>{bio ?? "No User Bio"}</p>
+    </section>
+  );
+};
+
+export default MainInfo;

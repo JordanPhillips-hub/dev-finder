681ce84faadce2c4190d478b872afebad60d7c77
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-29 11:33:09 -0600

    Set conditional window width for rendering the error message

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -7,32 +7,40 @@ interface SearchProps {
   errMsg: string;
 }
 
+const windowWidth = window.innerWidth;
 const SearchBar: React.FC<SearchProps> = ({ onChange, onSubmit, errMsg }) => {
   return (
-    <form onSubmit={onSubmit} className="relative mb-4 md:mb-6">
-      <label htmlFor="search" className="hidden">
-        Search
-      </label>
-      <GoSearch className="text-electricBlue absolute h-6 w-6 inset-y-5 left-2 md:left-8" />
-      <input
-        id="search"
-        type="text"
-        name="search"
-        onChange={onChange}
-        placeholder="Search Github username..."
-        className="placeholder:text-slateBlue placeholder:dark:text-white dark:bg-deepIndigo dark:text-white text-sm md:text-lg w-full h-16 pl-10 md:pl-20 rounded-2xl shadow-lg text-opacity-20 focus:outline-none"
-      />
-
-      <div className="absolute right-2 inset-y-3.5 md:inset-y-3">
-        <small className="text-red-500 text-xs font-bold">{errMsg}</small>
-        <button
-          type="submit"
-          className="bg-electricBlue text-white font-bold text-sm md:text-base rounded-lg py-2 px-4 ml-1.5 hover:bg-electricBlueHover focus:bg-electricBlueHover outline-none"
-        >
+    <>
+      <form onSubmit={onSubmit} className="relative mb-4 md:mb-6">
+        <label htmlFor="search" className="hidden">
           Search
-        </button>
-      </div>
-    </form>
+        </label>
+        <GoSearch className="text-electricBlue absolute h-6 w-6 inset-y-5 left-2 md:left-8" />
+        <input
+          id="search"
+          type="text"
+          name="search"
+          onChange={onChange}
+          placeholder="Search Github username..."
+          className="placeholder:text-slateBlue placeholder:dark:text-white dark:bg-deepIndigo dark:text-white text-sm md:text-lg w-full h-16 pl-10 md:pl-20 rounded-2xl shadow-lg text-opacity-20 focus:outline-none"
+        />
+
+        <div className="absolute right-2 inset-y-3.5 md:inset-y-3">
+          {windowWidth >= 768 && (
+            <small className="text-red-500 text-xs font-bold">{errMsg}</small>
+          )}
+          <button
+            type="submit"
+            className="bg-electricBlue text-white font-bold text-sm md:text-base rounded-lg py-2 px-4 ml-1.5 hover:bg-electricBlueHover focus:bg-electricBlueHover outline-none"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      {windowWidth < 768 && (
+        <small className="text-red-500 text-xs font-bold">{errMsg}</small>
+      )}
+    </>
   );
 };
 

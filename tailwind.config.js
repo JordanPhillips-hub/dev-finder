252d06b54dea1a4a7102d78ee0ebeaaf5ccb1f96
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-28 21:20:09 -0600

    Update colors to include dark mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,21 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
+        // Light Mode Colors
         lightestBlue: "#F6F8FF",
         slateBlue: "#697C9A",
         steelBlue: "#4B6A9B",
         electricBlue: "#0079FF",
         electricBlueHover: "#60ABFF",
         midnightBlue: "#2B3442",
+
+        // Dark Mode Colors
+        deepIndigo: '#1E2A47',
+        navy: '#141D2F'
       },
       fontSize: {
         "2.2xl": "1.625rem"
